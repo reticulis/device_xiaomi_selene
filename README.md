16749ded9539eca5b51f0abea037b54be17644c5
reticulis <reticulis@protonmail.com>
2023-06-09 12:36:54 +0200

    init commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Android device tree for Redmi 21061119AG (selene)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_selene-user
+add_lunch_combo omni_selene-userdebug
+add_lunch_combo omni_selene-eng
